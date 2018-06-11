992771c4f40b30282cc7d4f2068bb61330d892c1
Kuba Sułkowski <kubasulek2@gmail.com>
2018-06-11 16:27:33 +0200

    javascript basic slide-show added

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var slideIndex = 1;
+    var slides = document.querySelectorAll(".slide");
+    var cirlces = document.querySelectorAll('.circle');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        for(var i = 0; i < slides.length ; i++){
+            slides[i].style.display = "none";
+            }
+        slides[n-1].style.display ='block';
+
+    }
+
+    function changeSlide(n) {
+        slideIndex+=n;
+        if(slideIndex>slides.length){
+            slideIndex = 1
+        }else if(slideIndex<1){
+            slideIndex = 5
+        }
+        showSlides(slideIndex);
+
+    }
+    document.querySelector(".prev-button").addEventListener("click", function(e){
+        e.preventDefault();
+        changeSlide(-1)
+    });
+
+    document.querySelector(".next-button").addEventListener("click",function (e){
+        e.preventDefault();
+        changeSlide(1)
+    });
+
+    // function chooseSlide(n){
+    //
+    // }
+
 
  });
