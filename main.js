4ac1f152db9dfeab0a3c337d137f0f3ad4057357
Kuba Sukowski <kubasulek2@gmail.com>
2018-11-15 17:16:42 +0100

    javascript for slideshow finished

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     //zmienne plus pokazanie pierwszego slajdu
 
-    var slideIndex = 1;
-    var slides = document.querySelectorAll(".slide");
-    var circles = document.querySelectorAll('.circle');
+    let slideIndex = 1;
+    const slides = document.querySelectorAll(".slide");
+    const circles = document.querySelectorAll('.circle');
+    const prevButtons = document.querySelectorAll(".prev-button");
+    const nextButtons = document.querySelectorAll(".next-button");
+
+
     showSlides(slideIndex);
 
     //funkcje
@@ -20,30 +24,46 @@ document.addEventListener("DOMContentLoaded", function () {
         for(var i = 0; i < slides.length ; i++){
             slides[i].style.display = "none";
         }
+
         for(var i = 0; i < circles.length ; i++){
             circles[i].classList.remove("active");
         }
+
         slides[slideIndex-1].style.display ='block';
-        circles[slideIndex-1].classList.add("active")
+        circles[slideIndex-1].classList.add("active");
     }
 
-
     //event listenery
-    document.querySelector(".prev-button").addEventListener("click", function(e){
-        e.preventDefault();
-        changeSlide(-1)
-    });
 
-    document.querySelector(".next-button").addEventListener("click",function (e){
-        e.preventDefault();
-        changeSlide(1)
-    });
+
+    for(let i = 0; i < prevButtons.length; i++){
+
+        prevButtons[i].addEventListener("click", function(event){
+
+            event.preventDefault();
+            changeSlide(-1)
+        })
+
+    }
+
+    for(let i = 0; i < nextButtons.length; i++){
+
+        nextButtons[i].addEventListener("click", function(event){
+
+            event.preventDefault();
+            changeSlide(1)
+        })
+
+    }
 
     for(var i = 0; i < circles.length; i++){
+
         circles[i].onclick = function (a) {
             return function () {
-                var slide = a+1;
-                chooseSlide(slide)
+
+                let slide = a + 1;
+
+                chooseSlide(slide);
             }
         }(i);
     }
